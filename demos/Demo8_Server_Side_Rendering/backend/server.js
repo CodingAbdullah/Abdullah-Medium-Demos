e1483f6a9b5963113a7bc66b9994602830cc971f
Abdul <abdullahmd326@gmail.com>
2023-08-05 03:36:55 -0400

    Demo 8 : Code Complete

---
+++ demos/Demo8_Server_Side_Rendering/backend/server.js
@@ -0,0 +1,56 @@
+require("dotenv").config({ path: '.env' });
+const express = require("express");
+const axios = require("axios");
+
+// Spin up a Node server
+const app = express(); 
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening to PORT " + process.env.PORT);
+});
+
+// Setting Templating Engine to EJS
+app.set('view engine', 'ejs');
+
+// Setting location to folder containing files
+app.use(express.static(__dirname + 'public'));
+
+// Home EJS Page
+app.get("/", (req, res) => {
+    res.render('home', { year : new Date().getFullYear() });
+});
+
+// Data Fetched Information
+app.get("/api-information", (req, res) => {
+    // Setting options for API call
+    let options = {
+        method: 'GET',
+        headers: {
+            'accept' : 'application/json',
+            'content-type' : 'applcation/json'
+        }
+    }
+
+    axios.get('https://api.coingecko.com/api/v3/' + 
+    'coins/list?include_platform=false', options)
+    .then(response => {
+        // If successful, pass in the data from API call to render function
+        res.render('data', 
+            { 
+                coinData: response.data, 
+                isError: false, 
+                year: new Date().getFullYear() 
+            }
+        );
+    })
+    .catch(() => {
+        // If not successful, pass in the empty data and set error to true
+        res.render('data', 
+            { 
+                coinData: [], 
+                isError: true, 
+                year: new Date().getFullYear() 
+            }
+        );
+    });
+});
