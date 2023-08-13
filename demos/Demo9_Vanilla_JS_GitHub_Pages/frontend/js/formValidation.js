ecc89dd7871835f633b049b24d28577ba9992d4d
Abdul <abdullahmd326@gmail.com>
2023-08-13 01:09:37 -0400

    Demo 9 : Adding Vanilla JS content

---
+++ demos/Demo9_Vanilla_JS_GitHub_Pages/frontend/js/formValidation.js
@@ -0,0 +1,21 @@
+// Validate Form Submission
+const formValidation = () => {    
+    let inputValue = document.getElementById('inputValue').innerHTML;
+    let passwordValue = document.getElementById('passwordValue').innerHTML;
+
+    // Create a Regular Expression Representing what a Valid Email is for Comparison
+    let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    // Add an Event Listener to Button 
+    document.getElementById('formButton').addEventListener('click', () => {
+        if (!inputValue.match(emailRegex) && !passwordValue.length > 5) {
+            alert("Invalid email and password!");
+        }
+        else if (inputValue.match(emailRegex) && !passwordValue.length > 5) {
+            alert("Invalid password!");
+        }
+        else if (inputValue.match(emailRegex) && passwordValue.length > 5) {
+            alert("Email and password are both valid!!");
+        }
+    });
+}
