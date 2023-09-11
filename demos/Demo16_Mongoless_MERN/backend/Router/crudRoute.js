375b07f7c57a417d6aaf5ab5c03bdfa054e3dcd7
Abdul <abdullahmd326@gmail.com>
2023-09-11 15:21:01 -0400

    Demo 16: Backend complete with Sequelize, AWS RDS, Models/Controllers

---
+++ demos/Demo16_Mongoless_MERN/backend/Router/crudRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const UserController = require("../Controller/UserController");
+
+const router = express.Router();
+
+router.post("/create-user", UserController.createUser);
+router.post("/read-user", UserController.readUser);
+router.post("/update-user", UserController.updateUser);
+router.post("/delete-user", UserController.deleteUser);
+
+module.exports = router;
