65b9e94c231c793721ded3c24d54e18acd1783cc
Abdul <abdullahmd326@gmail.com>
2023-09-24 01:29:17 -0400

    Demo 19: Code complete

---
+++ demos/Demo19_AWS_S3_React/frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+// Navbar
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/"><b>AWS/React Deployed Pages</b></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/"><b>Login</b></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+      </nav>
+    )
+}
+
+export default Navbar;
---
+++ demos/Demo19_AWS_S3_React/frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Navbar from './Components/Navbar/Navbar';
+import LoginFormPage from './Components/LoginFormPage/LoginFormPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LoginFormPage />}></Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
