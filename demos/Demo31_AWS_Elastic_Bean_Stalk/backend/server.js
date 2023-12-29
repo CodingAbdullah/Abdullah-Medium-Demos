b262c04ea65ab92acf8b2228f252a067684ddfe2
Abdul <abdullahmd326@gmail.com>
2023-12-29 00:41:49 -0500

    Demo 31: AWS Elastic Beanstalk backend updated

--- demos/Demo31_AWS_Elastic_Bean_Stalk/backend/server.js
+++ demos/Demo31_AWS_Elastic_Bean_Stalk/backend/server.js
@@ -6,8 +6,8 @@ const crudRoute = require("./Router/crudRoute");
 const app = express(); // Spin up Node server
 
 // Listening to PORT 5000
-app.listen(5000, () => {
-    console.log("Listening to PORT 5000");
+app.listen(8080, () => {
+    console.log("Listening to PORT 8080");
 });
 
 app.use(express.json());
