39e126faefd16f7331947b7195e6915e2fa2ec14
CodingAbdullah <abdullahmd326@gmail.com>
2024-01-17 02:51:24 -0500

    Demo 32: Finalizing Navbar

--- demos/Demo32_AWS_Cloud_Networking/frontend/src/Components/Navbar/Navbar.jsx
+++ demos/Demo32_AWS_Cloud_Networking/frontend/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a style={{ padding: '1rem' }} className="navbar-brand" href="#/">React-Node.js-AWS S3 Demo</a>
+            <a style={{ padding: '1rem' }} className="navbar-brand" href="#/">AWS VPC Demo</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
