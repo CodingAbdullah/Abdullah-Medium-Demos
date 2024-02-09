909c2819385350dd3dbe4111b5ac172015359ab3
CodingAbdullah <abdullahmd326@gmail.com>
2024-02-09 01:46:54 -0500

    Demo 35: Demo complete, Medium Article link added

--- demos/Demo35_Maps_Sets_LRUCache/frontend/src/redux/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import toDoReducer from '../reducers/toDoReducer';
-
-// Pass in the reducers to the store
-export const store = configureStore({
-    reducer: {
-        'toDoList' : toDoReducer
-    }   
-});
