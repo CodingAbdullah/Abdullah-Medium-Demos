bbf9ea531390ae3559a41a79ae5f718019ca3dc1
CodingAbdullah <abdullahmd326@gmail.com>
2024-03-25 18:25:22 -0400

    Demo 40: Selenium Testing folder added

---
+++ demos/Demo40_Selenium_WebDriver_Testing/webapp/selenium_testing/src/selenium_testing/Test.java
@@ -0,0 +1,9 @@
+package demos.Demo40_Selenium_WebDriver_Testing.webapp.selenium_testing.src.selenium_testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
