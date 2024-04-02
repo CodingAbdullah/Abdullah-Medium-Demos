e66aac7c0336b743cb658be42c1be552ebac10a3
CodingAbdullah <abdullahmd326@gmail.com>
2024-04-02 00:56:19 -0400

    Demo 40: Refinements made to codebase

--- demos/Demo40_Selenium_WebDriver_Testing/webapp/src/Components/HomePage/HomePage.tsx
+++ demos/Demo40_Selenium_WebDriver_Testing/webapp/src/Components/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const HomePage: FC = () => {
     return (
         <div className='home-page'>
-            <h1>Welcome to the Selenium WebDriver Testing Application</h1>
+            <h1 style={{ marginTop: '1rem' }}>Selenium WebDriver Testing Application</h1>
             <p><i>Testing React applications using Selenium WebDriver</i></p>
         </div>
     )
