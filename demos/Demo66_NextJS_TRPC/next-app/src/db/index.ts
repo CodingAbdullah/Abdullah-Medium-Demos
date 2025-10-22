5823e5dfcd51ec524cf287eb4973aa965a1823b0
Abdullah Muhammad <abdullahmd326@gmail.com>
2025-10-22 00:21:35 -0400

    Delete demos/Demo66_NextJS_TRPC directory

--- demos/Demo66_NextJS_TRPC/next-app/src/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-
-const connectionString = process.env.DATABASE_URL!;
-
-export const client = postgres(connectionString);
-export const db = drizzle(client, { schema });
